39b773d12b59eecb21950931c023a57aa4a8812d
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-31 06:43:08 +1300

    webpack and server implemented

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var server = express();
+
+server.use(cors("*"));
+
+server.use(bodyParser.json());
+server.use(express.static(path.join(__dirname, "../public")));
+
+module.exports = server;
